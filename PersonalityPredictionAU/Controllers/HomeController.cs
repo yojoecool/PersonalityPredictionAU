eef4800777a1aec6cea418a4e8d8c801d0a0bafe
Bridges <Bridges@BRIDGES-PC>
2016-09-06 15:06:34 -0500

    Add project files.

---
+++ PersonalityPredictionAU/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PersonalityPredictionAU.Models;
+using System.Web.Script.Serialization;
+
+namespace PersonalityPredictionAU.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Personality Prediction";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult PostRetrieval()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View();
+            }
+            else return RedirectToAction("UnAuthenticated");
+        }
+
+        public ActionResult UnAuthenticated()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public String userPostsTxt(String response, String userID)
+        {
+            //string fileName = System.IO.Path.GetTempPath() + userID + "_posts.txt";
+            string fileName = AppDomain.CurrentDomain.BaseDirectory + "Temp\\" + userID + "_posts.txt";
+            //String allPosts = new JavaScriptSerializer().Deserialize<String>(response);
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(System.IO.File.Open(fileName, System.IO.FileMode.Create)))
+            {
+                file.Write(response);
+            }
+            return "/Temp/" + userID + "_posts.txt";
+        }
+    }
+}
---
+++ PersonalityPredictionAU/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalityPredictionAU.Startup))]
+namespace PersonalityPredictionAU
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
