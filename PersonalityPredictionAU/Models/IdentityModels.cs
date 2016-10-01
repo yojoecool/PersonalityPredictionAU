dbb0de64c523bf8feffe49ce86ee367e878b126d
yojoecool <yojoecool@gmail.com>
2016-10-01 00:44:00 -0400

    commit

--- PersonalityPredictionAU/Models/IdentityModels.cs
+++ PersonalityPredictionAU/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace PersonalityPredictionAU.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("PPDB", throwIfV1Schema: false)
+            : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
