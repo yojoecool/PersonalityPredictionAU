08117ec7210ecedd241efe34bcaf5c0a84668f93
jgaston93 <jgaston93@gmail.com>
2016-10-25 17:14:24 -0500

    authenticated api

--- TextAnalysisAPI/Controllers/HomeController.cs
+++ TextAnalysisAPI/Controllers/HomeController.cs
@@ -1,22 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace TextAnalysisAPI.Controllers
 {
-    [RequireHttps]
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-            if (User.Identity.IsAuthenticated)
-            {
-                ViewBag.Title = "Not Authenticated";
-            }
-
             return View();
         }
     }
