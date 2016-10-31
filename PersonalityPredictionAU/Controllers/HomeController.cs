0808de6b053a72be9312f1a3a695670220baa45c
Michael Pearce <mtp0013@auburn.edu>
2016-10-30 20:31:38 -0500

    API calls in HomeController

--- PersonalityPredictionAU/Controllers/HomeController.cs
+++ PersonalityPredictionAU/Controllers/HomeController.cs
@@ -12,6 +12,8 @@ namespace PersonalityPredictionAU.Controllers
     [RequireHttps]
     public class HomeController : Controller
     {
+        private PersonalityPredictionDBEntities _context = new PersonalityPredictionDBEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -60,9 +62,87 @@ namespace PersonalityPredictionAU.Controllers
         }
 
         [HttpPost]
-        public String storeScores(String scores)
+        public String retrieveScores(String content)
         {
-            return scores;
+            // Keys for accessing API
+            String api_key = "57f4699a7ba60a05aad03fcf";
+            String api_secret_key = "oJ1bk5DlRmDkzneGKvr459Zs56Z1DNrEzyrpdnlhCS0";
+            String url = "https://textanalysisapi.azurewebsites.net/api/textanalysis?text=\"" + content + "\"";
+
+            // Setting up the request to the API
+            // TODO Add exception handling for 400,404,etc. responses
+            WebClient request = new WebClient();
+            //request.Headers.Add("Content-Type", "application/json");
+            //request.Headers.Add("X-API-KEY", api_key);
+            //request.Headers.Add("X-API-SECRET-KEY", api_secret_key);
+            //byte[] responseArray = request.UploadData("https://app.receptiviti.com/api/person", "POST", System.Text.Encoding.ASCII.GetBytes(content));
+            byte[] responseArray = request.DownloadData(url);
+
+            // Preprocess the response to target the scores
+            String responseText = System.Text.Encoding.ASCII.GetString(responseArray);
+            int start = responseText.IndexOf("\"liwc_scores\"");
+            int end = responseText.IndexOf("\"content_date\"");
+            responseText = responseText.Substring(start + 16, end - start - 20);
+
+            // Iteratively retrieve the scores
+            List<LiwcScoreModel> Scores = new List<LiwcScoreModel>();
+            while (responseText.Length > 0)
+            {
+                LiwcScoreModel mod = new LiwcScoreModel();
+                int subStart = responseText.IndexOf("\"") + 1;  // Each category is delimited by quotes
+                int subEnd = responseText.IndexOf(":") - 1;     // The : is a good landmark for the end of the category
+                mod.Category = responseText.Substring(subStart, subEnd - subStart); // Retrieve the Category
+                if (mod.Category == "categories")   // This entry contains a list of the individual, non-aggregate scores
+                {
+                    responseText = responseText.Substring(subEnd + 4);  // Repurpose the string to ignore the "categories" label and parse the scores again
+                    continue;
+                }
+                subStart = subEnd + 3;  // Move subStart to the beginning of the score itself
+                subEnd = (responseText.Contains(",") ? responseText.IndexOf(",") : responseText.Length);    // Find the end of the score
+                mod.Score = (float) Convert.ToDouble(responseText.Substring(subStart, subEnd - subStart));  // Save the score
+                Scores.Add(mod);
+                responseText = (responseText.Length == subEnd ? "" : responseText.Substring(subEnd + 1));   // Progress the response text to the next category
+            }
+
+            this.addToDB(Scores);
+            
+            return responseText;
+        }
+
+        private void addToDB(List<LiwcScoreModel> Scores)
+        {
+            try
+            {
+                // Find the user in the account table
+                string username = User.Identity.Name;
+                Account user = _context.Accounts.FirstOrDefault(a => a.Email == username);
+
+                // Remove previous scores
+                foreach (CategoryScore catScore in user.CategoryScores.ToList())
+                {
+                    _context.CategoryScores.Remove(catScore);
+                }
+
+                // Add in new scores
+                foreach (LiwcScoreModel score in Scores)
+                {
+                    _context.CategoryScores.Add(new CategoryScore()
+                    {
+                        Account = user,
+                        Category = _context.Categories.FirstOrDefault(c => c.Name == score.Category),
+                        SourceId = 1,
+                        Score = score.Score
+                    });
+                }
+
+                // Save changes to database
+                _context.SaveChanges();
+                return;
+            }
+            catch
+            {
+                return;
+            }
         }
     }
 }
