6611d124b360d3f4cedd2eeb8b83e6dc5347e8a6
jgaston93 <jgaston93@gmail.com>
2016-11-27 12:57:24 -0600

    CSV Upload for dictionary

--- PersonalityPredictionAU/Controllers/DictionariesController.cs
+++ PersonalityPredictionAU/Controllers/DictionariesController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using PersonalityPredictionAU.Models;
+using System.IO;
 
 namespace PersonalityPredictionAU.Controllers
 {
@@ -122,6 +123,53 @@ namespace PersonalityPredictionAU.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            // make sure file isn't empty
+            if(file.ContentLength > 0)
+            {
+                PersonalityPredictionDBEntities _context = new PersonalityPredictionDBEntities();
+                Dictionary dictionary = new Dictionary();
+                dictionary.Name = "LiwcDictionary";
+                dictionary.Date = DateTime.Now;
+                _context.Dictionaries.Add(dictionary);
+                StreamReader sr = new StreamReader(file.InputStream);
+                while (sr.Peek() != -1)
+                {
+                    string line = sr.ReadLine();
+                    string[] tokens = line.Split(',');
+                    int i = 0;
+                    Category category = new Category();
+                    category.Name = tokens[i];
+                    category.Dictionary = dictionary;
+                    _context.Categories.Add(category);
+                    category.CategoryTypeId = 18;
+                    for (i = 1; i < tokens.Length; i++)
+                    {
+                        Word word = new Word();
+                        word.Name = tokens[i];
+                        category.Words.Add(word);
+                        _context.Words.Add(word);
+                    }
+                }
+                sr.Close();
+                try
+                {
+                    _context.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    int j = 0;
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
